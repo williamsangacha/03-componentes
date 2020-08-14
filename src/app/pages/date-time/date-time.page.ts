88549bd250d538c3628212515d7e666b987d109a
williamsangacha <46944201+williamsangacha@users.noreply.github.com>
2020-08-14 12:46:50 -0500

    commid date-time-part1

---
+++ src/app/pages/date-time/date-time.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-time',
+  templateUrl: './date-time.page.html',
+  styleUrls: ['./date-time.page.scss'],
+})
+export class DateTimePage implements OnInit {
+
+  fechaNaci: Date = new Date();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cambioFecha(event){
+      console.log("ionChange", event);
+      console.log('Date', new Date( event.detail.value));
+  }
+  
+}
