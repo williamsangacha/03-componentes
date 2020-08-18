3032881b89545d823fd0e5cabf74ac19f720c1fa
williamsangacha <46944201+williamsangacha@users.noreply.github.com>
2020-08-18 11:39:34 -0500

    commid loading-dismiss

---
+++ src/app/pages/loading/loading.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.page.html',
+  styleUrls: ['./loading.page.scss'],
+})
+export class LoadingPage implements OnInit {
+
+  constructor( private loadingCtrl: LoadingController) { }
+
+  loading : any;
+
+  ngOnInit() {
+    this.presentLoading('Loading');
+
+    setTimeout(()=>{
+      this.loading.dismiss();
+    }, 1500);
+
+  }
+
+  async presentLoading( message: string) {
+    this.loading = await this.loadingCtrl.create({
+      cssClass: 'my-custom-class',
+      message: message,
+      // duration: 2000
+    });
+    return this.loading.present();
+  }
+
+}
