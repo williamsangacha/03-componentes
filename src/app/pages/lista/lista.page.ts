fe9864c05749fc921af9b5b1814ccff3a0a77c8e
williamsangacha <46944201+williamsangacha@users.noreply.github.com>
2020-08-18 10:55:45 -0500

    commid list-sliding

--- src/app/pages/lista/lista.page.ts
+++ src/app/pages/lista/lista.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista',
@@ -8,6 +9,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./lista.page.scss'],
 })
 export class ListaPage implements OnInit {
+
+  @ViewChild('lista') lista : IonList;
 
   usuarios : Observable<any>;
 
@@ -17,4 +20,18 @@ export class ListaPage implements OnInit {
     this.usuarios=this.dataService.getUsers();
   }
 
+  favorite(user){
+    console.log('favorite', user);
+    this.lista.closeSlidingItems();
+  }
+
+  share(user){
+    console.log('share', user);
+    this.lista.closeSlidingItems();
+  }
+
+  borrar(user){
+    console.log('borrar', user);
+    this.lista.closeSlidingItems();
+  }
 }
