5d6a605423d5a7ddbfc695f4a7285593073aac6a
williamsangacha <46944201+williamsangacha@users.noreply.github.com>
2020-08-21 14:06:52 -0500

    commid tabs

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/tabs/contact'
+  },
+  {
+    path: '',
+    component: TabsPage,
+    children:[
+      {
+        path:'account',
+        loadChildren: '../avatar/avatar.module#AvatarPageModule'
+      },
+      {
+        path:'contact',
+        loadChildren: '../lista/lista.module#ListaPageModule'
+      },
+      {
+        path:'settings',
+        loadChildren: '../infiniti-scroll/infiniti-scroll.module#InfinitiScrollPageModule'
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
